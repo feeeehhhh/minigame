e746cedfc5de878c97fc282ef119d21c39ba207a
Fehh Rocha <indorpac011@gmail.com>
2024-06-12 16:52:50 -0300

    feat and fix : corrige as teclas acertadas e incorretas e adiciona musica

--- minigame/src/components/Game.tsx
+++ minigame/src/components/Game.tsx
@@ -1,32 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import LinearProgress from '@mui/material/LinearProgress'
+
+import correto from '../assets/Som de acerto.mp3'
 
 interface GameProps {
   nivel: 'Easy' | 'Hard'
 }
 
 const Easy = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i))
-
 const Hard = [...Easy, ...Array.from({ length: 10 }, (_, i) => i.toString())]
 
 const Game: React.FC<GameProps> = ({ nivel }) => {
   const [caracteresSorteados, setCaracteresSorteados] = useState<string[]>([])
   const [tempoRestante, setTempoRestante] = useState<number>(0)
   const [resultado, setResultado] = useState<string>('')
-  const [tempoInicial, setTempoInicial] = useState<number>(0)
+  const [indiceAtual, setIndiceAtual] = useState<number>(0)
+  const [coresAnteriores, setCoresAnteriores] = useState<string[]>(
+    Array(6).fill(''),
+  )
+
+  const gameRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (gameRef.current) {
+      gameRef.current.focus()
+    }
+  }, [])
 
   const sortearCaracteres = (array: string[]) => {
-    const caracteres = []
-    for (let i = 0; i < 6; i++) {
-      const indiceSorteado = Math.floor(Math.random() * array.length)
-      caracteres.push(array[indiceSorteado])
+    const caracteresSorteados = []
+    const quantidadeCaracteres = 6
+    for (let i = 0; i < quantidadeCaracteres; i++) {
+      const indiceAleatorio = Math.floor(Math.random() * array.length)
+      const caractereSorteado = array[indiceAleatorio]
+      caracteresSorteados.push(caractereSorteado)
     }
-    setCaracteresSorteados(caracteres)
+
+    setCaracteresSorteados(caracteresSorteados)
   }
 
   useEffect(() => {
     const tempoInicial = nivel === 'Easy' ? 10 : 3
-    setTempoInicial(tempoInicial)
+
     setTempoRestante(tempoInicial)
     const arrayParaSortear = nivel === 'Easy' ? Easy : Hard
     sortearCaracteres(arrayParaSortear)
@@ -43,41 +58,60 @@ const Game: React.FC<GameProps> = ({ nivel }) => {
     }
   }, [tempoRestante])
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    const teclaPressionada = event.key.toUpperCase()
-    if (caracteresSorteados.includes(teclaPressionada)) {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    event.preventDefault()
+    const tecla = event.key.toUpperCase()
+    const esperado = caracteresSorteados[indiceAtual]
+    const newCoresAnteriores = [...coresAnteriores]
+    const audioCorreto = new Audio()
+
+    if (tecla === esperado) {
+      newCoresAnteriores[indiceAtual] = 'bg-green-500'
       setResultado('Caractere correto!')
     } else {
+      newCoresAnteriores[indiceAtual] = 'bg-red-500'
       setResultado('Caractere incorreto!')
     }
+
+    setCoresAnteriores(newCoresAnteriores)
+
+    if (indiceAtual === 5) {
+      // Último caractere
+      if (tecla === esperado) {
+        setResultado('Você completou a sequência corretamente!')
+      }
+    } else {
+      setIndiceAtual((prevIndice) => prevIndice + 1)
+    }
   }
-  const progress = (tempoRestante / tempoInicial) * 100
 
   return (
     <div
-      className="text-yellow-50 min-w-[500px] flex flex-col items-center w-full"
-      onKeyPress={handleKeyPress}
+      ref={gameRef}
+      className="text-yellow-50 min-w-[500px] flex flex-col items-center w-full outline-none"
+      onKeyDown={handleKeyDown}
       tabIndex={0}
     >
       <div className="p-4 w-full">
-        <p>Pressione as teclas abaixo no seu teclado:</p>
-        <ul className="flex gap-8">
+        <div className="flex gap-8">
           {caracteresSorteados.map((caractere, index) => (
-            <li
+            <div
               key={index}
-              className="flex  bg-slate-50 text-slate-950 rounded-lg text-2xl p-2 px-4 font-bold"
+              className={`flex text-slate-950 rounded-lg text-2xl p-2 px-4 font-bold ${
+                index === indiceAtual ? 'bg-blue-500 text-white' : ''
+              } ${coresAnteriores[index]}`}
             >
               {caractere}
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
       <div>
         <p>Tempo restante: {tempoRestante} segundos</p>
         <LinearProgress
           variant="determinate"
-          value={progress}
-          className=" py-1 rounded-3xl"
+          value={(indiceAtual / 6) * 100}
+          className="py-1 rounded-3xl"
         />
         <p>{resultado}</p>
       </div>
